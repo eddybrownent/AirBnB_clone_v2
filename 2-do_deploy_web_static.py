8ad2bc96a56c419dcb817df32207583a1c27ad76
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-09-07 12:34:45 +0300

    Wrote a Fabric script based on the file 1-pack_web_static.py that distributes an archive to the web servers,

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+This fabric script desploys an archive to web servers
+"""
+from fabric.api import env, run, put
+import os
+
+
+env.hosts = ['54.197.21.126', '54.236.56.228']
+env.user = 'ubuntu'
+env.key_filename = ['/root/.ssh/my_ssh_key']
+
+
+def do_deploy(archive_path):
+    """
+    distributes an archive to webservers
+    """
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        """
+        Upload archive to /tmp/ directory in web server
+        """
+        put(archive_path, '/tmp/')
+
+        """
+        extracting the archive to /data/web_static/releases/ directory
+        """
+        archive_name = os.path.basename(archive_path)
+        extract_file = "/data/web_static/releases/{}".format(
+                archive_name.split('.')[0])
+
+        run("mkdir -p {}".format(extract_file))
+        run("tar -xzf /tmp/{} -C {}".format(archive_name, extract_file))
+
+        """
+        Deleting the archive from the server
+        """
+        run("rm /tmp/{}".format(archive_name))
+
+        """
+        Removin the current symbolic link
+        """
+        c_link = "/data/web_static/current"
+        run("rm -f {}".format(c_link))
+
+        """
+        Creating new symbolic link pointing to the new page
+        """
+        run("ln -s {} {}".format(extract_file, c_link))
+
+        return True
+    except Exception:
+        return False
