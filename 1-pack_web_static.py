7eb74a8181db6114f7a9d2071c30c3ae4128aa45
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-09-07 08:22:27 +0300

    fixed the PEP requirements errors

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,10 +12,10 @@ def do_pack():
     Generates a .tgz archive from the contents of the web_static
     """
     try:
-        # Create the "versions" directory if it doesn't exist
+        # Create the versions directory if it doesn't exist
         local("mkdir -p versions")
 
-        # seting the time 
+        # seting the time
         time = datetime.now().strftime("%Y%m%d%H%M%S")
 
         # creating the name
@@ -29,5 +29,5 @@ def do_pack():
         # Check if archive was created successfully and return the path
         archive_path = "versions/{}".format(archive_name)
         return archive_path
-    except Exception as e:
+    except Exception:
         return None
