f17f4978518035ddbd85418024d2699d495f334e
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-09-09 10:43:41 +0300

    This pupupet file that sets up your web servers for the deployment of web_static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,53 +1,56 @@
-# 101-setup_web_static.pp
-
-# Install nginx if not installed
+# Define the Nginx package
 package { 'nginx':
-  ensure => installed,
+  ensure => 'installed',
 }
 
-# Create directories if they don't exist
-file { '/data/web_static/releases/test/':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0755',
+# Ensure /data directory exists with the right permissions
+file { '/data':
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0755',
+  require => Package['nginx'],
 }
 
-file { '/data/web_static/shared/':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0755',
+# Ensure /data/web_static directory structure exists
+file { ['/data/web_static', '/data/web_static/releases', '/data/web_static/shared']:
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0755',
+  require => File['/data'],
 }
 
-# Create an HTML file
+# Create an HTML file in /data/web_static/releases/test
 file { '/data/web_static/releases/test/index.html':
-  ensure  => file,
+  ensure  => 'file',
   owner   => 'ubuntu',
   group   => 'ubuntu',
   mode    => '0644',
   content => "<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>\nThis is a test Holberton School\n</body>\n</html>\n",
+  require => File['/data/web_static/releases/test'],
 }
 
 # Create or recreate a symbolic link
 file { '/data/web_static/current':
-  ensure => link,
-  target => '/data/web_static/releases/test/',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  ensure  => 'link',
+  target  => '/data/web_static/releases/test/',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  require => File['/data/web_static/releases/test/index.html'],
 }
 
-# Update nginx configuration
+# Update the Nginx configuration
 file_line { 'nginx_hbnb_static':
-  ensure => present,
-  path   => '/etc/nginx/sites-enabled/default',
-  line   => '        location /hbnb_static/ { alias /data/web_static/current/; }',
-  before => Service['nginx'],
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-enabled/default',
+  line    => '        location /hbnb_static/ { alias /data/web_static/current/; }',
+  require => Service['nginx'],
 }
 
-# Restart nginx
+# Ensure the Nginx service is running and enabled
 service { 'nginx':
-  ensure    => running,
+  ensure    => 'running',
   enable    => true,
   subscribe => File_line['nginx_hbnb_static'],
 }
