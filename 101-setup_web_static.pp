44fa65b305f7bbbfe71d67eed817b96156cb6a61
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-09-09 10:55:40 +0300

    This pupupet file that sets up your web servers for the deployment of web_static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,4 +1,4 @@
-# Define the Nginx package
+# Install Nginx if not already installed
 package { 'nginx':
   ensure => 'installed',
 }
@@ -13,10 +13,11 @@ file { '/data':
 }
 
 # Ensure /data/web_static directory structure exists
-exec { 'create_web_static_dirs':
-  command => 'sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/',
-  creates => ['/data/web_static/releases/test/', '/data/web_static/shared/'],
-  path    => ['/bin', '/usr/bin'],
+file { ['/data/web_static', '/data/web_static/releases', '/data/web_static/shared']:
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0755',
   require => File['/data'],
 }
 
@@ -27,18 +28,27 @@ file { '/data/web_static/releases/test/index.html':
   group   => 'ubuntu',
   mode    => '0644',
   content => "<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>\nThis is a test Holberton School\n</body>\n</html>\n",
-  require => Exec['create_web_static_dirs'],
+  require => File['/data/web_static/releases/test'],
 }
 
-# Create or recreate a symbolic link
-exec { 'create_web_static_link':
-  command => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
-  creates => '/data/web_static/current',
-  path    => ['/bin', '/usr/bin'],
+# Ensure /data/web_static/current symbolic link exists and is up to date
+file { '/data/web_static/current':
+  ensure  => 'link',
+  target  => '/data/web_static/releases/test/',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
   require => File['/data/web_static/releases/test/index.html'],
+  before  => Service['nginx'],
 }
 
-# Update the Nginx configuration
+# Give ownership of the /data/ folder to the ubuntu user and group recursively
+exec { 'change_ownership':
+  command => 'chown -R ubuntu:ubuntu /data/',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => "test \$(stat -c %U /data) != 'ubuntu'",
+}
+
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 file_line { 'nginx_hbnb_static':
   ensure  => 'present',
   path    => '/etc/nginx/sites-enabled/default',
@@ -46,9 +56,10 @@ file_line { 'nginx_hbnb_static':
   require => Service['nginx'],
 }
 
-# Ensure the Nginx service is running and enabled
+# Restart Nginx to apply the updated configuration
 service { 'nginx':
   ensure    => 'running',
   enable    => true,
   subscribe => File_line['nginx_hbnb_static'],
 }
+
