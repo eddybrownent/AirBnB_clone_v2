749e24b60ca3003fe9bbe446e1f82c59f1ecd58c
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-09-26 18:39:44 +0300

    Cities by states  output correct

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" THis script that starts a Flask web application
-listening on 0.0.0.0, port 5000
-"""
+""" This script that starts a Flask web application"""
 
 from flask import Flask, render_template
 from models import storage
