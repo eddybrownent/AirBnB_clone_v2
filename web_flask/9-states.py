efd72b6f2e7d0d9a07b0ae4c0b115dcafa3f4972
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-09-26 23:11:16 +0300

    States and State with ID in route

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,40 +1,56 @@
 #!/usr/bin/python3
-"""Importing Flask to run the web app"""
+"""
+Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: Display a list of all State objects sorted by name.
+    /states/<id>: Display a list of cities in a specific State.
+"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 
-
+""" Creating a Flask web application instance """
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def close(self):
+@app.route('/states', strict_slashes=False)
+def states_list():
     """
-    Method to close the session
+    Display a list of all State objects sorted by name.
+    """
+    state_list = []
+    states = storage.all("State").values()
+    for state in states:
+        if isinstance(state, State):
+            state_list.append(state)
+    return render_template('9-states.html', mode='all', state_list=state_list)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_cities(id):
+    """
+    Display a list of cities in a specific State.
+    """
+    state_list = []
+    states = storage.all("State").values()
+    for state in states:
+        if state.id == id:
+            return render_template('9-states.html',
+                                   state_list=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """
+    Remove the current SQLAlchemy session.
     """
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-def state():
-    """
-    Displays a html page with states
-    """
-    states = storage.all(State)
-    return render_template('9-states.html', states=states, mode='all')
-
-
-@app.route('/states/<id>', strict_slashes=False)
-def state_by_id(id):
-    """
-    Displays a html page with citiess of that state
-    """
-    for state in storage.all(State).values():
-        if state.id == id:
-            return render_template('9-states.html', states=state, mode='id')
-    return render_template('9-states.html', states=state, mode='none')
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == "__main__":
+    """ Runs the Flask app """
+    app.run(host="0.0.0.0", port=5000)
