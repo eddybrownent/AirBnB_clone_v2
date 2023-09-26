7050481642a87c7c953b377c75396e91e96621c9
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-09-26 20:07:53 +0300

    Cities by states  output correct

--- models/state.py
+++ models/state.py
@@ -12,14 +12,15 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='all, delete-orphan',  foreign_keys='City.state_id')
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan',
+                              foreign_keys='City.state_id')
     else:
         name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes state"""
         super().__init__(*args, **kwargs)
-
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
